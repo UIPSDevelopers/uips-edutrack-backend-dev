ab7b61a12d99d0b1e370c0bf9f2cea33473b4dca
JC <jctanmaquera@gmail.com>
2026-04-22 09:40:18 +0400

    add locationsRoutes

---
+++ src/routes/locationsRoutes.js
@@ -0,0 +1,23 @@
+import express from "express";
+import {
+  addLocation,
+  getAllLocations,
+  updateLocation,
+  deleteLocation,
+} from "../../controllers/propertytagging/locationsController.js";
+
+const router = express.Router();
+
+// CREATE
+router.post("/", addLocation);
+
+// READ
+router.get("/", getAllLocations);
+
+// UPDATE
+router.put("/:id", updateLocation);
+
+// DELETE
+router.delete("/:id", deleteLocation);
+
+export default router;
